7e0b18489a28b14fe6487c29d483682fa6c9a544
Codkashh <as3733542@gmail.com>
2025-02-12 13:47:59 +0530

    updation in FE

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const aiRoutes = require("./routes/ai.routes.js")
+const cors = require('cors');
+
 
 const app = express();
+
+app.use(cors())
 app.use(express.json())
 
 app.get('/',(req, res) => {
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,34 @@ import { useState, useEffect } from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 import Editor from "react-simple-code-editor"
 import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios'
+import './App.css'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [ code, setCode ] = useState(` function sum() {
+  return 1 + 1
+}`)
+
+  const [ review, setReview ] = useState(``)
+
+  useEffect(() => {
+    prism.highlightAll()
+  }, [])
+
+  async function reviewCode() {
+    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    setReview(response.data)
+  }
 
   return (
-    <main>
-      <div className="left">
-        <div className="code">
-        <Editor
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
               value={code}
               onValueChange={code => setCode(code)}
               highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
@@ -23,16 +43,23 @@ function App() {
                 width: "100%"
               }}
             />
+          </div>
+          <div
+            onClick={reviewCode}
+            className="review">Review</div>
         </div>
-        <div className="review">
-          Review
-        </div>
-      </div>
-      <div className="right">
+        <div className="right">
+          <Markdown
 
-      </div>
-    </main>
+            rehypePlugins={[ rehypeHighlight ]}
+
+          >{review}</Markdown>
+        </div>
+      </main>
+    </>
   )
 }
+
+
 
 export default App
